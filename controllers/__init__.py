581c502253d4444c5f74cbbd861c5095fa529db2
WJurgiel <wjurgiel@student.agh.edu.pl>
2026-01-07 21:56:55 +0100

    Simulation refactor

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,3 +1,4 @@
 from .system_controller import SystemManager
 from .materials_controller import MaterialsManager
 from .forces_controller import ForcesManager
+from .simulation_controller import SimulationManager
